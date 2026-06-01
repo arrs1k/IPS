31f1243c600cdec577967f94107331327f719707
Arsenii Volchenkov <amvolchenkov@edu.hse.ru>
2026-06-01 15:12:34 +0300

    small addition

--- etc_full_learning_link.py
+++ etc_full_learning_link.py
@@ -1,1 +1,2 @@
 #Ознакомиться с полным кодом можно по ссылке https://www.kaggle.com/code/mrarsik/notebook8faaf1411c/notebook?scriptVersionId=323549759
+#Полный ноутбук файлом с гитхаба открыть не получается
